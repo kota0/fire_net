dd0817762b19b364bcf50f55708fe889e27b8647
kota0 <kota.mac07@gmail.com>
2022-07-02 14:50:23 +0900

    viewファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :posts, only: [:index, :create] do
+  resources :posts, only: [:index, :new] do
     resources :comments, only: [:show, :new, :edit, :delete]
   end
 end

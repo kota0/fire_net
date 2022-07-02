4ee1005288c15f521d08e1de3f27275accc6bd5e
kota0 <kota.mac07@gmail.com>
2022-07-02 14:22:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+  resources :posts, only: [:index, :create] do
+    resources :comments, only: [:show, :new, :edit, :delete]
+  end
 end

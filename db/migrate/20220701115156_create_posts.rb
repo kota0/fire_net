f2ac5db760134c755879bdac8c1de41201b55893
kota0 <kota.mac07@gmail.com>
2022-07-02 12:05:20 +0900

    modelの作成（deviseも）

---
+++ db/migrate/20220701115156_create_posts.rb
@@ -0,0 +1,17 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.timestamps
+      t.integer    :user_id
+      t.blob       :image
+      t.blob       :video
+      t.integer    :area1_id,   null: false
+      t.integer    :area2_id,   null: false 
+      t.text       :mark
+      t.integer    :jnjury_id,  null: false
+      t.integer    :escape_id,  null: false
+      t.integer    :help_id,    null: false
+      t.text       :content,    null: false
+    end
+  end
+end

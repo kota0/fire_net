a680c961b1cb40cb92f9cb01445c4843fe4bb957
kota0 <kota.mac07@gmail.com>
2022-07-05 12:01:59 +0900

    first_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+  root to: 'posts#index'
   resources :posts, only: [:index, :new] do
     resources :comments, only: [:show, :new, :edit, :delete]
   end

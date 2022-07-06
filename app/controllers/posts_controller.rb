1abe7ad98b0cdd97b2944c919897eb224be92f72
kota0 <kota.mac07@gmail.com>
2022-07-06 11:48:47 +0900

    投稿機能の修正１

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,19 @@ class PostsController < ApplicationController
     @post = Post.new 
   end
 
+  def create
+    post.create(post_params)
+  end
+     
+
   def index
+    @posts = Post.all
   end
 
-  
+
+  private
+  def post
+    params.require(:post).permit(:image, :video, :area1, :area2, :mark, :jujury, :escape, :help, :content)
+  end
 end
 

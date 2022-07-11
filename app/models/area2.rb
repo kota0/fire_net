cb5d3ef470b0df924d17c47d8c356f8bcf61da30
kota0 <kota.mac07@gmail.com>
2022-07-11 09:14:55 +0900

    投稿機能の修正

--- app/models/area2.rb
+++ app/models/area2.rb
@@ -1,6 +1,7 @@
 class Area2 < ActiveHash::Base
  self.data = [
- { id: 1, name: '___'}
+ { id: 1, name: '___'},
+ { id: 2, name: '街'}
 
 
  ]
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   
   
-  belongs_to :comments
+  # belongs_to :comments
   belongs_to :user
   has_one_attached :image
 end

9e52e64a00f78382cc1e4d781276effa3ee66dfa
kota0 <kota.mac07@gmail.com>
2022-07-29 17:03:05 +0900

    ハンバーガーメニューの実装

---
+++ app/javascript/post/post_index.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector("#navArea");
+let btm = document.querySelector(".toggle-btm");
+let mask = document.querySelector("#mask");
+
+btn.onclick = () => {
+  nav.classList.toggle("open");
+};
+
+mask.onclick = () => {
+  nav.classList.toggle("open");
+};
+

affd0129d7f35d8507517da4576affb38ace5265
kota0 <kota.mac07@gmail.com>
2022-08-07 00:00:27 +0900

    プルダウンの絞り込みの実装4

--- app/javascript/packs/area.js
+++ app/javascript/packs/area.js
@@ -1,29 +1,228 @@
-$(function(){ 
+$(function () {
+  $('#post_state_id').on('change', function () {
+      let TokyoStateId = $('.tokyo-state-box ').val();
 
-  
-  $('#post_state_id').on('change',function(){
-  
-  
-    let TokyoStateId = $('.tokyo-state-box ').val();
-    let TokyoAreaId = $('.tokyo-area-box ').val();
+      $('.tokyo-area-box').val('');
+      $('.tokyo-area-box option').css('display', 'block');
+      $('#post_area_id').css('display', 'block');
 
- 
-   if (TokyoStateId == "3" ) {
-    
-    // $('.tokyo-area-box option[value]').css('display', 'block');
+  switch (TokyoStateId){
+     case '1':
+        $('.tokyo-area-box option[value ]').css('display', 'none');
+            i++;
+     break;
+           
+      
 
-    $('.tokyo-area-box option[value]').css('display', 'block');
-    $('.tokyo-area-box option[value]').each(function(i) {
-      if (i>38) {
-        $('.tokyo-area-box option[value = ' +(i)+ ']').css('display', 'none');
+     case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
 
-        // ここの記述で選択したid以外を非表示にする
-        var r = $('option:tokyo-state-box').val();
-        $(r != i).css('display', 'block');
+      case '3':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 10 || i >= 20) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
+
+      case '2':
+        $('.tokyo-area-box option[value]').each(function (i) {
+          if (i < 30 || i >= 45) {
+              $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+          }
+          i++;
+          })
+      break;
 
 
-       }
-      })
-    }
-  })
-});
+
+
+
+
+
+
+
+          }
+          })
+        });
+
+
+
+
+
+
+
+
+
+
+      // if (TokyoStateId === "1") {
+      //   $('.tokyo-area-box option[value ]').css('display', 'none');
+      //       i++;
+      //  }
+
+      // if (TokyoStateId === "3") {
+      //   $('.tokyo-area-box option[value]').each(function (i) {
+      //       if (i < 10 || i >= 15) {
+      //           $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+      //       }
+      //       i++;
+      //   })
+      // }
+
+
+
+
+
+
+
+      // if (TokyoStateId === "3") {
+      //     $('.tokyo-area-box option[value]').each(function (i) {
+      //         if (i < 10 || i >= 20) {
+      //             $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+      //         }
+      //         i++;
+      //     })
+      //   }
+      // } else {
+      //     $('.tokyo-area-box option[value]').each(function (i) {
+      //         if (i >= 10 && i < 20) {
+      //             $('.tokyo-area-box option[value = ' + (i) + ']').css('display', 'none');
+      //         }
+      //         i++;
+      //     })
+      // }
+//    }
+//  });

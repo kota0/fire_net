17fec77219788009d75abfbe4824bcaee759233d
kota0 <kota.mac07@gmail.com>
2022-09-09 23:51:29 +0900

    動画の保存のプレビュー表示３

--- app/javascript/packs/video.js
+++ app/javascript/packs/video.js
@@ -1,28 +1,38 @@
-function changeFile(file){
 
-  const isVideoExt = (file) => {
-    fileName = fileBox.substring(fileBox.lastIndexOf('.'))
-    if (fileName.toUpperCase().match(/\.(mp4)$/i)) {
-      return true
-    }
-    if (fileName.toUpperCase().match(/\.(avi)$/i)) {
-      return true
-    }
-    if (fileName.toUpperCase().match(/\.(fiv)$/i)) {
-      return true
-    }
-    if (fileName.toUpperCase().match(/\.(mov)$/i)) {
-      return true
-    }
-    if (fileName.toUpperCase().match(/\.(wmv)$/i)) {
-      return true
-    }
-    return false
+
+
+  window.changeFile = function changeFile(file){
+
+    const fileBox = document.getElementById('post-video');
+    const msg = document.getElementById('msg');
+
+    fileBox.addEventListener("change", (e) => {
+      if (window.File) {
+        var inputFile = fileBox.files[0];
+      }
+    
+    const isVideoExt = (file) => {
+      fileName = fileBox.substring(fileBox.lastIndexOf('.'))
+      if (fileName.toUpperCase().match(/\.(mp4)$/i)) {
+        return true
+      }
+      if (fileName.toUpperCase().match(/\.(avi)$/i)) {
+        return true
+      }
+      if (fileName.toUpperCase().match(/\.(fiv)$/i)) {
+        return true
+      }
+      if (fileName.toUpperCase().match(/\.(mov)$/i)) {
+        return true
+      }
+      if (fileName.toUpperCase().match(/\.(wmv)$/i)) {
+        return true
+      }
+      return false
   }
-    msg.innerText = '動画が選択されました';
-}
+ })
+    msg.innerText = '１件の動画が選択されています';
+};
 
-  let fileBox = document.getElementById('post-video');
-  fileBox.addEventListener('change', changeFile);
-  const msg = document.getElementById('msg');
 
+  

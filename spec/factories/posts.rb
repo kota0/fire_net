454657d2f948b0d8e673b4676b0302424c48302a
kota0 <kota.mac07@gmail.com>
2022-09-23 18:40:36 +0900

    postのexampleの整理

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :post do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    state                {}
+    area                 {}
+    escape               {}
+    help                 {}
+    injury               {}
+    association :user 
   end
 end 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+end
+
+describe '新規投稿' do
+    context '新規投稿できるとき' do
+      it '画像と動画のどちらかのみが添付されていて、state,area,escape,help,injuryが指定されていれば投稿できる' do
+      end
+    end
+
+    context '新規登録できないとき' do
+      it 'stateが未指定では投稿できない' do
+      end
+
+      it 'areaが未指定では投稿できない' do
+      end
+
+      it 'escapeが未指定では投稿できない' do
+      end
+
+      it 'helpが未指定では投稿できない' do
+      end
+
+      it 'injuryが未指定では投稿できない' do
+      end
+
+      it '画像か動画どちらかが添付されていないと投稿できない' do
+      end
+
+      it '画像か動画どちらも添付されていると投稿できない' do
+      end
+
+      it '画像は４枚までしか投稿できない' do
+      end
+
+      it '画像のtypeは「image/png, image/jpg, image/jpeg」しか投稿できない' do
+      end
+
+      it '画像は1.5megabytes以上は投稿できない' do
+      end
+
+      it 'ユーザーが紐付いていなければ投稿できない' do
+      end
+
+    end
+  end
 end

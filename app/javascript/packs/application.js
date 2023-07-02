bb7fc75f8399d6fdd4020012e1ba033d1e92ba3d
藤枝　洸太 <97730296+kota0@users.noreply.github.com>
2023-07-02 23:13:20 +0900

    詳細コメントの表示の設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery3
-//= require jquery_ujs
-//= require_tree .
- require("jquery")
- require("./slick")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -17,7 +12,9 @@ import '@fortawesome/fontawesome-free/js/all';
  Rails.start()
  Turbolinks.start()
  ActiveStorage.start()
-
+ 
+ import "jquery"
+ import "./slick"
  import "./hamburger"
  import "./area"
  import "./slick"
